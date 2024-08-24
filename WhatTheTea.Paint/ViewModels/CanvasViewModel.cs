6fad529ea36bd917befb210266a265fbadebb98c
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-08-24 19:19:49 +0300

    Create prototype

---
+++ WhatTheTea.Paint/ViewModels/CanvasViewModel.cs
@@ -0,0 +1,83 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.UI.Xaml.Shapes;
+
+using System.Diagnostics;
+
+using Windows.UI;
+
+namespace WhatTheTea.Paint.ViewModels
+{
+    internal enum DrawingState
+    {
+        None,
+        Drawing,
+        Erasing
+    }
+    public partial class CanvasViewModel : ObservableObject
+    {
+        private static SolidColorBrush BlackBrush = new SolidColorBrush(Color.FromArgb(255,0,0,0));
+        private DrawingState drawingState = DrawingState.None;
+        private PointerPoint? deltaPoint;
+
+        public CanvasViewModel()
+        {
+        }
+
+        public void Canvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (sender is Canvas canvas)
+            {
+                drawingState = DrawingState.Drawing;
+            }
+        }
+
+        public void Canvas_PointerMove(object sender, PointerRoutedEventArgs e)
+        {
+            if (sender is Canvas canvas && drawingState == DrawingState.Drawing)
+            {
+                var point = e.GetCurrentPoint(canvas);
+                Debug.WriteLine($"{point.Timestamp} - {point.Position.X} {point.Position.Y}");
+
+                var element = new Line()
+                {
+                    Stroke = BlackBrush,
+                    Fill = BlackBrush,
+                    StrokeThickness = 2,
+
+                    X2 = point.Position.X,
+                    Y2 = point.Position.Y
+                };
+
+                if (deltaPoint is not null)
+                {
+                    element.X1 = deltaPoint.Position.X;
+                    element.Y1 = deltaPoint.Position.Y;
+                }
+                else
+                {
+                    element.X1 = point.Position.X;
+                    element.Y1 = point.Position.Y;
+                }
+
+                canvas.Children.Add(element);
+                deltaPoint = point;
+            }
+        }
+
+        public void Canvas_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (sender is Canvas canvas)
+            {
+                drawingState = DrawingState.None;
+                deltaPoint = null;
+            }
+        }
+    }
+}
---
+++ WhatTheTea.Paint/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+using DependencyPropertyGenerator;
+
+using Microsoft.UI.Xaml.Controls;
+
+using WhatTheTea.Paint.ViewModels;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WhatTheTea.Paint.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    [DependencyProperty<CanvasViewModel>("ViewModel")]
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
